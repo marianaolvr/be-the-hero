e5b98fbbb7213c19bcc25aff2d96b130380c8149
Mariana Oliveira <olvr.mariana@gmail.com>
2020-03-28 16:30:04 -0300

    Cors for development

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors()); //como estamos em desenvolvimento, fica apenas assim para todas as aplicações frontend possam acessar esse backend. 
 app.use(express.json()); // antes das rotas, falando para o express ir para o corpo da requisição e transformar o json em um objeto do JS
-
 app.use(routes);
-
 app.listen(3333);
+
+
+// cors em produção ficaria assim:
+
+app.use(cors({
+    origin: 'http://meuapp.com' //o endereço que vai poder acessar a aplicação, frontend hospedado
+}));
