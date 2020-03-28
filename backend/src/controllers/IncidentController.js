a5b6470d7d034e94bb88ba7a74a11efebe461b60
Mariana Oliveira <olvr.mariana@gmail.com>
2020-03-28 16:25:49 -0300

    Pagination of incidentes request

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -3,7 +3,28 @@ const connection = require('../database/connection');
 module.exports = {
 
     async index(request, response) {
-        const incidents = await connection('incidents').select('*');
+
+        const { page = 1 } = request.query; //paginação da listagem
+
+        const [count] = await connection('incidents') // contador: query que vai retornar a quantidade de casos
+            .count();
+
+        const incidents = await connection('incidents')
+            .join('ongs', 'ongs.id', '=', 'incidents.ong_id') //relacionar dados de duas tabelas
+            .limit(5) //limita para 5 o número de retornos de incidentes 
+            .offset((page - 1) * 5) //pular 5 registros para cada hora
+            .select
+            ([ //campos que eu estou buscando no DB
+                'incidents.*',
+                'ongs.name',
+                'ongs.email',
+                'ongs.whatsapp',
+                'ongs.city',
+                'ongs.uf'
+            ]); 
+
+        response.header('X-Total-Count', count['count(*)']);
+
         return response.json(incidents);
 
     },
@@ -28,7 +49,7 @@ module.exports = {
         const ong_id = request.headers.authorization; //pega o id da ong que cadastrou o incidente
 
         const incident = await connection('incidents').where('id', id).select('ong_id').first();
-        if(incident.ong_id !== ong_id) { 
+        if (incident.ong_id !== ong_id) {
             return response.status(401).json({ error: 'Operation not permited.' }); //não autorizado
         }
         //se deu tudo certo, se ele passou, deleto do banco
