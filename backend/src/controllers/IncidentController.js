815415325a76e184570e2cfeaf47ec63c1420209
Mariana Oliveira <olvr.mariana@gmail.com>
2020-03-28 14:06:59 -0300

    Routes for create and list incidents

---
+++ backend/src/controllers/IncidentController.js
@@ -0,0 +1,26 @@
+const connection = require('../database/connection');
+
+module.exports = {
+
+    async index(request, response) {
+        const incidents = await connection('incidents').select('*');
+        return response.json(incidents);
+
+    },
+
+    async create(request, response) {
+        const { title, description, value } = request.body;
+        //cabeçalho da requisição, onde fica a autenticação
+        const ong_id = request.headers.authorization;
+
+        const [id] = await connection('incidents').insert({
+            title,
+            description,
+            value,
+            ong_id,
+        });
+
+        return response.json({ id });
+    }
+};
+
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,18 +2,20 @@ const express = require('express');
 
 //importa o controler com a função de criar
 const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
 
 const routes = express.Router(); //desacoplando o módulo de rotas express em uma nova variavel
 
-
-//rota para listar todas as ongs do banco de dados
+//ROTAS ONG
+//listar
 routes.get('/ongs', OngController.index);
-
-//rota de cadastro
+//cadastrar
 routes.post('/ongs', OngController.create);
 
-
-
-
+//ROTAS INCIDENTS
+//listar
+routes.get('/incidents', IncidentController.index);
+//cadastrar
+routes.post('/incidents', IncidentController.create);
 
 module.exports = routes; //exportar para usar no index.js
