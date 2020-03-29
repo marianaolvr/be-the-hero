4ce763dddc44b06ba08aec36eae6c25af024ff47
Mariana Oliveira <olvr.mariana@gmail.com>
2020-03-28 21:14:33 -0300

    Routes and style for Profile (one ONG incidents) and new incidents

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 // Link substitui o <a> para não recarregar toda a página SPA
 import { FiLogIn } from 'react-icons/fi';
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident';
 
 export default function Routes() {
     return (
@@ -13,6 +15,8 @@ export default function Routes() {
         <Route path="/" exact component={Login} />
         {/* Exact porque o react lê apenas a barra e isso interfere nas outras rotas. todas cairíam na primeira */}
         <Route path="/register" component={Register} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/incidents/new" component={NewIncident} />
 
         </Switch>
 
